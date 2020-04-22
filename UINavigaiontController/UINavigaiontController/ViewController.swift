b176eded07109399432e9dc5a1ebcdee7c6d8acf
Shohei.Mizuno <32non23ca@gmail.com>
2020-04-22 16:34:40 -0700

    submit

---
+++ UINavigaiontController/UINavigaiontController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UINavigaiontController
+//
+//  Created by user169300 on 4/22/20.
+//  Copyright © 2020 user169300. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
