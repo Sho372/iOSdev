58d4d41951c664cd6895a3201d82890ab7d7f666
Shohei.Mizuno <32non23ca@gmail.com>
2020-05-03 13:22:29 -0700

    WIP

---
+++ PersonalityQuiz/PersonalityQuiz/IntroductionViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by user169300 on 4/30/20.
+//  Copyright © 2020 user169300. All rights reserved.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
