2f25cabad4213631a99b3d8326f9a863890b4559
mk1990h2 <mikass111@yahoo.co.jp>
2020-05-21 18:31:38 -0700

    WIP [Restarurant] Part Five

--- Restaurant/Restaurant/MenuItemDetailViewController.swift
+++ Restaurant/Restaurant/MenuItemDetailViewController.swift
@@ -9,13 +9,14 @@
 import UIKit
 
 class MenuItemDetailViewController: UIViewController {
+    
+    var menuItem: MenuItem!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
 
     /*
     // MARK: - Navigation
